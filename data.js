63dadaf7dbd49bfc0a539a29f145a04c1cf10c6f
Abdulaziz Alswaji <abdulaziz.alswaji@gmail.com>
2022-04-01 16:25:35 +0300

    data & details files

---
+++ data.js
@@ -0,0 +1,76 @@
+data = {
+    values: {
+        customers: {
+            details: 0,
+            sales: {
+                details: 1,
+                values: {
+                    q1: 2.6,
+                    q2: 4.9,
+                    q3: 1.1,
+                    q4: 5.9
+                }
+            },
+
+
+            new_customers: {
+                details: 2,
+                values: {
+                    q1: 129,
+                    q2: 321,
+                    q3: 889,
+                    q4: 576
+                }
+            },
+
+            refund: {
+                details: 3,
+                values: {
+                    q1: 3.7,
+                    q2: 3.8,
+                    q3: 6.0,
+                    q4: 1.2
+                }
+            },
+        },
+
+        employee: {
+            details: 4,
+            new_employee: {
+                details: 5,
+                values: {
+                    q1: 1.9,
+                    q2: 4.9,
+                    q3: 1.1,
+                    q4: 2.1
+                }
+            },
+
+            resign_employee: {
+                details: 6,
+                values: {
+                    q1: 231,
+                    q2: 75,
+                    q3: 23,
+                    q4: 21
+                }
+            }
+        }
+
+
+
+    },
+
+    trend: {
+        sales: {
+            details: 1,
+            values: [6576, 7537, 9876, 12223, 6567, 9087, 6099, 2375, 7767, 4476, 1112, 4334]
+        },
+
+
+        refund: {
+            details: 3,
+            values: [757, 1537, 476, 1223, 167, 607, 899, 6375, 567, 246, 812, 434]
+        }
+    }
+}
---
+++ details.js
@@ -0,0 +1,39 @@
+details = {
+    0: {
+        label: 'Customers',
+        icon: 'noun-people-32407-FFFFFF.svg'
+    },
+
+    1: {
+        label: 'Sales',
+        icon: 'noun-purchase-1433685-FFFFFF.svg',
+        unit: 'Millions SAR'
+    },
+
+    2: {
+        label: 'New customers',
+        icon: 'noun-add-user-81368-FFFFFF.svg',
+    },
+
+    3: {
+        label: 'Refund',
+        icon: 'noun-refund-1106395-FFFFFF.svg',
+        unit: 'Thounsands SAR'
+    },
+
+    4: {
+        label: 'Employee',
+        icon: 'noun-employees-186484-FFFFFF.svg'
+    },
+
+    5: {
+        label: 'New employee',
+        icon: 'noun-user-162961-FFFFFF.svg',
+    },
+
+    6: {
+        label: 'Resigned employee',
+        icon: 'noun-exit-1472515-FFFFFF.svg',
+    },
+
+}
